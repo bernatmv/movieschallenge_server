262096f33e374b6b9d1906376d3d9a1fc0516af5
Bernat Martinez <bmartinezv@cirsa.com>
2015-06-19 12:05:49 +0200

    (feat) added connection to mongoDB using mongoose

--- src/server/server.js
+++ src/server/server.js
@@ -1,12 +1,17 @@
 // load the express package and create our app
 import express from 'express';
 import path from 'path';
+import db from 'mongoose';
 
 var app = express(),
 	router = express.Router();
 
+// connect to DB
+db.connect('mongodb://localhost/lovely');
+
 // test
 app.get('/', (req, res) => {
+	console.log(db.users.find());
 	res.send('at last 4!');
 });
 
@@ -28,6 +33,18 @@ router.param('username', (req, res, next, username) => {
 });
 
 // creates routes for the app
+router.route('/login')
+	.get((req, res) => {
+		res.send('login action method get');
+	})
+	.post((req, res) => {
+		res.send('login action method post');
+	});
+
+router.get('/user/:username', (req, res) => {
+	res.send(`retrieve ${req.params.username} data`);
+});
+
 router.get('/user/:username', (req, res) => {
 	res.send(`retrieve ${req.params.username} data`);
 });
