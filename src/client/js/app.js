57fc51a5355ab9a8786b8ef6f2145d314b52d440
Bernat Martinez <bernatmv@gmail.com>
2015-06-24 23:58:20 +0200

    (feat) added routing for angular

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -3,20 +3,14 @@ angular
 	.controller('mainController', function() {
 		// bind "this" to "vm" (view-model)
 		var vm = this;
-		// all variables defined on thi will be available on the view
-		vm.message = 'Yo!';
-		vm.facts = [
-			{ name: 'fuck', color: 'yellow', time: 10 },
-			{ name: 'sleep', color: 'green', time: 1000 },
-			{ name: 'eat', color: 'red', time: 20 },
-		];
-		vm.factsData = {};
-		vm.addFact = function() {
-			vm.facts.push({
-				name: vm.factsData.name,
-				color: vm.factsData.color,
-				time: vm.factsData.time,
-			});
-			vm.factsData = {};
-		}
+		// all variables defined on "this" will be available on the view
+		vm.message = 'main';
+	})
+	.controller('questionsController', function() {
+		var vm = this;
+		vm.message = 'questions';
+	})
+	.controller('contactController', function() {
+		var vm = this;
+		vm.message = 'contact';
 	});
