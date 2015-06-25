1807e544b43cc68417c2a1826ecfe26d552fb7db
Bernat Martinez <bmartinezv@cirsa.com>
2015-06-25 18:17:29 +0200

    (feat) added authentication service

--- src/client/js/controllers/mainController.js
+++ src/client/js/controllers/mainController.js
@@ -1,12 +1,31 @@
+'use strict';
+
 angular
-	.module('ngApp.main', ['ngApp.authenticationService'])
-	.controller('mainController', function(Authenticate) {
+	.module('ngApp.main', ['ngApp.authService'])
+	.controller('mainController', function($rootScope, $location, Auth) {
 		// bind "this" to "vm" (view-model)
 		var vm = this;
-		// access the API
-		Authenticate
-			.login()
-			.then(function(response) {
-				vm.message = response.data.token;
-			});
+		// check if the user is logged in
+		vm.loggedIn = Auth.isLoggedIn();
+		// get user info on route change
+		$rootScope.$on('$routeChangeStart', function() {
+			vm.loggedIn = Auth.isLoggedIn();
+			Auth.getUser()
+				.success(function(data) {
+					vm.user = data;
+				});
+		});
+		// login handler
+		vm.doLogin = function() {
+			Auth.login(vm.loginData.username, vm.loginData.password)
+				.success(function(data) {
+					$location.path('/questions');
+				});
+		}
+		// lohout handler
+		vm.doLogout = function() {
+			Auth.logout();
+			vm.user = {};
+			$location.path('/home');
+		}
 	});
--- src/client/js/services/authenticationService.js
+++ src/client/js/services/authenticationService.js
@@ -1,13 +1,61 @@
-angular.module('ngApp.authenticationService', [])
-	.factory('Authenticate', function($http) {
-		// create factory
-		var factory = {};
-
-		// authenticate the user and get the token
-		factory.login = function() {
-			return $http.post('/api/authenticate', { username: 'berni', password: 'hanabi' });
-		}
-		
-		// return the factory
-		return factory;
+angular.module('ngApp.authService', [])
+	.factory('AuthToken', function($window) {
+		return {
+			getToken: function() {
+				return $window.localStorage.getItem('token');
+			},
+			setToken: function(token) {
+				if (token) {
+					$window.localStorage.setItem('token', token);	
+				}
+				else {
+					$window.localStorage.removeItem('token');
+				}
+			}
+		};
+	})
+	.factory('Auth', function($http, $q, AuthToken) {
+		return {
+			login: function(username, password) {
+				// get the token and save it
+				return $http.post('/api/authenticate', { 
+						username: username, 
+						password: password 
+					})
+					.success(function(data) {
+						console.log(data);
+						AuthToken.setToken(data.token);
+						return data;
+					});
+			},
+			logout: function() {
+				// clear the token
+				AuthToken.setToken();
+			},
+			isLoggedIn: function() {
+				return (AuthToken.getToken()) ? true : false;
+			},
+			getUser: function() {
+				return (AuthToken.getToken()) ? $http.get('/api/me', { cache: true }) : $q.reject({ message: 'No token available' });
+			}
+		};
+	})
+	.factory('AuthInterceptor', function($q, $location, AuthToken) {
+		return {
+			request: function(config) {
+				var token = AuthToken.getToken();
+				// if the token exists, add it to the headers
+				if (token) {
+					config.headers['x-access-token'] = token;
+				}
+				return config;
+			},
+			responseError: function(response) {
+				if (response.status === 403) {
+					AuthToken.setToken();
+					$location.path('/home');
+				}
+				return $q.reject(response);
+			}
+		};
 	});
