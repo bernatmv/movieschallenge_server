6b226d268d053809adc87827399444e804762395
Bernat Martinez <bernatmv@gmail.com>
2015-07-12 23:31:38 +0200

    (feat) implemented logic for answering question

--- src/server/api/game/game.controller.js
+++ src/server/api/game/game.controller.js
@@ -64,13 +64,75 @@ class GameController {
 	}
 
 	answerQuestion (req, res) {
-		res.send({ game: req.params.gameId, question: req.params.questionId, correct: req.body.correct });
+		const currentUser = req.decodedToken.username;
+		// get the game
+		GameModel.findById(req.params.gameId, (err, game) => {
+			if (err) {
+				res.send(err);
+			}
+			else {
+				const whoIsPlayer = (currentUser == game.players.challenger.username) ? 'challenger' : 'challenged';
+				const whoIsRival = (currentUser == game.players.challenger.username) ? 'challenged' : 'challenger';
+				const rivalPlayer = game.players[whoIsRival].username
+				// check that is the user's turn
+				if (game.thisTurn === req.decodedToken.username) {
+					// get the question being answered
+					QuestionModel.findById(req.params.questionId, (err, question) => {
+						if (err) {
+							res.send(err);	
+						}
+						else {
+							// if the answer is correct
+							if (question.correctAnswer == req.body.answer) {
+								// add play to the play array
+								games.plays.push({ question: question._id, category: question.category, difficulty: question.difficulty, correct: true });
+								// change last play date
+								game.lastPlay = new Date();								
+								// update category progress
+								game.players[whoIsPlayer].categoriesProgress[question.category]++;
+								// add question to questions answered
+								game.players[whoIsPlayer].questionsAnswered.push(question._id);
+								// if needed load the category star question
+
+								// if needed end the game and set the winner
+								
+							}
+							// if the answer is not correct, switch turn
+							else {
+								// add play to the play array
+								game.plays.push({ question: question._id, category: question.category, difficulty: question.difficulty, player: currentUser, correct: false });
+								// increment turn
+								game.turn++;
+								// change turn
+								game.thisTurn = rivalPlayer;
+								// change last play date
+								game.lastPlay = new Date();
+								// if the category progress is at 3 and the question is failed (it was a question for the star), reset progress to 0
+								if (game.players[whoIsPlayer].categoriesProgress[question.category] == 3) {
+									game.players[whoIsPlayer].categoriesProgress[question.category] = 0;
+								}
+								// add question to questions answered
+								game.players[whoIsPlayer].questionsAnswered.push(question._id);
+							}
+							// save the game and return the updated register
+							game.save((err, reg) => {
+								(err) ?	res.send(err) : res.json(reg);
+							});
+						}
+					});
+				}
+				else {
+					res.send({ success: false, message: 'Not the current user turn' });
+				}
+			}
+		});
+		res.send({ game: req.params.gameId, question: req.params.questionId, correct: req.body.correct, answer: req.body.answer, position: req.body.position });
 	}
 
 	static calculateNotCompletedCategories(progress) {
 		var categories = [];
 		for (var i = 0, j = progress.length; i < j; i++) {
-			if (progress[i] < 3) {
+			if (progress[i] < 4) {
 				categories.push(i + 1);
 			}
 		}
