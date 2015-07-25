63cccee017cac7fd2de7c2f74c59efe87de5a12b
Bernat Martinez <bernatmv@gmail.com>
2015-07-26 01:09:50 +0200

    (fix) change port to 8080

--- src/server/config/config.js
+++ src/server/config/config.js
@@ -1,5 +1,5 @@
 export default {
-	port: process.env.PORT || 80,
+	port: process.env.PORT || 8080,
 	//mongodb: 'mongodb://localhost/lovely',
 	mongodb: 'mongodb://admin:hanabi@ds055822.mongolab.com:55822/movieschallenge',
 	secret: 'hanabiarefireflowersthatbloomintheskywherethereisnolimitandeverythingispossible',
