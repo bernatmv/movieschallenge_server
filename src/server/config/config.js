9b67bc163736410e45380756039c3b7fa62cdd53
Bernat Martinez <bernatmv@gmail.com>
2015-07-26 01:01:44 +0200

    (feat) added connection to mongolab db

--- src/server/config/config.js
+++ src/server/config/config.js
@@ -1,5 +1,6 @@
 export default {
-	port: process.env.PORT || 8080,
-	mongodb: 'mongodb://localhost/lovely',
+	port: process.env.PORT || 80,
+	//mongodb: 'mongodb://localhost/lovely',
+	mongodb: 'mongodb://admin:hanabi@ds055822.mongolab.com:55822/movieschallenge',
 	secret: 'hanabiarefireflowersthatbloomintheskywherethereisnolimitandeverythingispossible',
 }
